6d50b498d6377b44310fbd4428ea1d9bb13b90c5
RomaBilka <roman_biliak@i.ua>
2025-05-24 14:07:37 +0300

    add docker compose

--- frontend/openapi-ts.config.ts
+++ frontend/openapi-ts.config.ts
@@ -2,19 +2,9 @@ import { defineConfig } from "@hey-api/openapi-ts";
 import { defaultPlugins } from "@hey-api/openapi-ts";
 
 export default defineConfig({
-  client: "@hey-api/client-fetch",
   input: "../shared/api/Veloci.Web.json",
   output: "src/api/client",
+  client: "@hey-api/client-fetch",
   experimentalParser: true,
-  plugins: [
-    ...defaultPlugins,
-    // {
-    //   name: "@hey-api/sdk",
-    //   transformer: true,
-    // },
-    // {
-    //   name: "@hey-api/transformers",
-    //   dates: true,
-    // },
-  ],
+  plugins: [...defaultPlugins],
 });
